e5748863d6efe30f8969aee6fb626446d5fbf91a
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-02-19 18:11:57 -0600

    Changing url

--- client/script.js
+++ client/script.js
@@ -99,7 +99,7 @@ const handleSubmit = async (e) => {
 
   //Fetch data from server -> bot's response
   /* Sending a POST request to the server. */
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-6m7q.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
